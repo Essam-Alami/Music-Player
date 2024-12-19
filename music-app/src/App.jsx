d7209539ee6502028f48705c46c097d33783a693
VampyrFangs <testezmonclic@gmail.com>
2024-12-19 21:12:57 +0100

    .

--- music-app/src/App.jsx
+++ music-app/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import './App.css'
-import Player from '../Player.jsx'
+import { useState } from 'react';
+import './App.css';
+import Player from '../Player';
 
 function App() {
 
