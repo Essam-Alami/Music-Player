acda87a857d508a022ef5603ba6306debc9bef6f
VampyrFangs <testezmonclic@gmail.com>
2024-12-25 18:28:50 +0100

    .

--- music-app/src/App.jsx
+++ music-app/src/App.jsx
@@ -3,23 +3,15 @@ import MusicPlayer from './components/MusicPlayer';
 import './App.css';
 import { MusicProvider } from './context/MusicContext';
 
-function App() {
-  return (
-    <div className="app">
-      <div className="container">
-        <h1>Music Player</h1>
-        <MusicPlayer />
-      </div>
-    </div>
-    
-  );
-}
-
+// Main App Component
 function App() {
   return (
     <MusicProvider>
       <div className="app">
-        <MusicPlayer />
+        <div className="container">
+          <h1>Music Player</h1>
+          <MusicPlayer />
+        </div>
       </div>
     </MusicProvider>
   );
